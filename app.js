d21a6260f91dce64203d72b02b17a8a3d56891dc
asaelrios <45483152+asaelrios@users.noreply.github.com>
2019-07-29 21:42:13 -0700

    Update app.js

--- app.js
+++ app.js
@@ -4,8 +4,22 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 const { handleFatalError } = require('./util')
+const mysql = require('mysql')
 
 app.use(express.static('public'))
+
+var connection = mysql.createConnection({
+  connectionLimit : 10,
+  host            : 'localhost',
+  user            : 'root',
+  password        : 'acl12345',
+  database        : 'SENSORS'
+  })
+
+connection.connect(function(err){
+  if(err)throw err
+  console.log('Connected')
+  })
 
 io.on('connection', (socket) => {
   console.log('user connected')
@@ -20,6 +34,31 @@ io.on('connection', (socket) => {
   })
 
   socket.on('send-metrics', (data) => {
+    data = data.toString()
+
+    let obj = null
+    try {
+      obj = JSON.parse(data)
+    } catch (e) {
+      return
+    }
+
+    var sql = "INSERT INTO DATA (FLS, FRS, RLS, RRS, STATE) VALUES (?, ?, ?, ?, ?)"
+
+    connection.query(sql,
+      [
+        obj.FLS,
+        obj.FRS,
+        obj.RLS,
+        obj.RRS,
+        obj.STATE
+      ],
+      (err, results) => {
+        if (err) {
+          handleError(err)
+          return
+        }
+      })
     console.log(data)
     io.sockets.emit('send-metrics-brodcast', data)
   })
